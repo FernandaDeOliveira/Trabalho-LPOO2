6699998400a7ed1cd425b0fa4439e9f03d6303bf
FernandaDeOliveira <pessoal.fernandaoliveira@gmail.com>
2015-12-02 21:48:20 -0200

    repositorios

---
+++ RotaBus/RotaBus.Repository/MesRepositorio.cs
@@ -0,0 +1,73 @@
+﻿using MySql.Data.MySqlClient;
+using RotaBus.Connection;
+using RotaBus.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotaBus.Repository
+{
+    public class MesRepositorio
+    {
+        Conexao conn = new Conexao();
+        MySqlCommand cmm = new MySqlCommand();
+        StringBuilder sql;
+
+   
+
+        public IEnumerable<Mes> GetAll()
+        {
+            List<Mes> meses = new List<Mes>();
+
+            sql = new StringBuilder();
+            sql.Append("SELECT * ");
+            sql.Append("FROM mes  ");
+            cmm.CommandText = sql.ToString();
+
+            MySqlDataReader dr = conn.ExecutarConsulta(cmm);
+
+            while (dr.Read())
+            {
+                meses.Add(new Mes
+                {
+                    id = (int)dr["id"],
+                    nome = (string)dr["nome"]
+                   
+
+                });
+            }
+            return meses;
+        }
+
+
+        public Mes GetOne(int pId)
+        {
+            sql = new StringBuilder();
+            sql.Append("SELECT * FROM meses d ");
+            sql.Append("WHERE d.id = @id");
+
+            cmm.Parameters.AddWithValue("@id", pId);
+
+            cmm.CommandText = sql.ToString();
+
+            MySqlDataReader dr = conn.ExecutarConsulta(cmm);
+
+            dr.Read();
+
+            Mes editar = new Mes
+            {
+                id = (int)dr["id"],
+                nome = (string)dr["nome"]
+              
+
+            };
+
+            return editar;
+        }
+
+      
+    }
+
+}
---
+++ RotaBus/RotaBus.Repository/SemanaRepositorio.cs
@@ -0,0 +1,72 @@
+﻿using MySql.Data.MySqlClient;
+using RotaBus.Connection;
+using RotaBus.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotaBus.Repository
+{
+    public class SemanaRepositorio
+    {
+
+        Conexao conn = new Conexao();
+        MySqlCommand cmm = new MySqlCommand();
+        StringBuilder sql;
+
+
+
+        public IEnumerable<Semana> GetAll()
+        {
+            List<Semana> semana = new List<Semana>();
+
+            sql = new StringBuilder();
+            sql.Append("SELECT * ");
+            sql.Append("FROM semana  ");
+            cmm.CommandText = sql.ToString();
+
+            MySqlDataReader dr = conn.ExecutarConsulta(cmm);
+
+            while (dr.Read())
+            {
+                semana.Add(new Semana
+                {
+                    id = (int)dr["id"],
+                    dia = (string)dr["dia"]
+
+
+                });
+            }
+            return semana;
+        }
+
+
+        public Semana GetOne(int pId)
+        {
+            sql = new StringBuilder();
+            sql.Append("SELECT * FROM semana d ");
+            sql.Append("WHERE d.id = @id");
+
+            cmm.Parameters.AddWithValue("@id", pId);
+
+            cmm.CommandText = sql.ToString();
+
+            MySqlDataReader dr = conn.ExecutarConsulta(cmm);
+
+            dr.Read();
+
+           Semana editar = new Semana
+            {
+                id = (int)dr["id"],
+                dia = (string)dr["dia"]
+
+
+            };
+
+            return editar;
+        }
+
+    }
+}
