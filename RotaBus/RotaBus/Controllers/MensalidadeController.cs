c28db0381224bd2ffd03c97d890c90e9d200a000
milenacardoso <milena3cardoso@hotmail.com>
2015-12-05 23:16:02 -0200

    Controller e Views - Mensalidade

---
+++ RotaBus/RotaBus/Controllers/MensalidadeController.cs
@@ -0,0 +1,50 @@
+﻿using RotaBus.Core;
+using System.Web.Mvc;
+
+namespace RotaBus.Controllers
+{
+    public class MensalidadeController : Controller
+    {
+        MensalidadeRepositorio mRepositorio = new MensalidadeRepositorio();
+        RotaRepositorio rRepositorio = new RotaRepositorio();
+
+        // GET: Mensalidade
+        public ActionResult Index()
+        {        
+            var mensalidades = mRepositorio.GetAll();
+            return View(mensalidades);
+        }
+
+        public ActionResult Create()
+        {
+            //carrega as classificacoes numa viewbag para carregar a select option
+            ViewBag.Rotas = rRepositorio.GetAll();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Mensalidade pMensalidade)
+        {
+            mRepositorio.Create(pMensalidade);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int id)
+        {
+            var mensalidade = mRepositorio.GetOne(id);
+            ViewBag.Rotas = rRepositorio.GetAll();
+            return View(mensalidade);
+        }
+        [HttpPost]
+        public ActionResult Update(Mensalidade pMensalidade)
+        {
+            mRepositorio.Update(pMensalidade);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            mRepositorio.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
